df4b3d133f014e7d6c30f15327585f7db228b3ad
HEECHANG7832 <facee22av@naver.com>
2021-04-29 07:54:08 +0900

    test Application.java

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,10 +2,13 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+ public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
